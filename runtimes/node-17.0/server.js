2c7ce6fe9134268ed950513be860a68869d22cf9
Matej Bačo <matejbaco2000@gmail.com>
2022-02-11 11:31:29 +0000

    Revert new changes to node server

--- runtimes/node-17.0/server.js
+++ runtimes/node-17.0/server.js
@@ -2,13 +2,10 @@ const path = require("path");
 const micro = require("micro");
 const { json, send } = require("micro");
 
-const DEFAULT_PATH = '/usr/code';
-const DEFAULT_FILE = 'index.js';
-
 const server = micro(async (req, res) => {
     const body = await json(req);
 
-    if (req.headers['x-internal-challenge'] !== process.env['INTERNAL_RUNTIME_KEY']) {
+    if (req.headers[`x-internal-challenge`] !== process.env['INTERNAL_RUNTIME_KEY']) {
         return send(res, 401, { code: 401, message: 'Unauthorized' });
     }
 
@@ -18,37 +15,26 @@ const server = micro(async (req, res) => {
         payload: body.payload ?? {},
     };
 
-    let isResponded = false;
     const response = {
-        send: (text, status = 200) => {
-            isResponded = true;
-            send(res, status, text);
-        },
-        json: (json, status = 200) => {
-            isResponded = true;
-            send(res, status, json);
-        },
+        send: (text, status = 200) => send(res, status, text),
+        json: (json, status = 200) => send(res, status, json),
     };
-
     try {
-        let userFunction = require(path.join(body.path ?? DEFAULT_PATH, body.file ?? DEFAULT_FILE));
+        let userFunction = require(path.join(body.path, body.file));
 
         if (!(userFunction || userFunction.constructor || userFunction.call || userFunction.apply)) {
-            throw new Error('Function not found.');
+            throw new Error("User function is not valid.")
         }
-
-        await userFunction(request, response);
-
-        if(!isResponded) {
-            send(res, 200, {
-                code: 200,
-                message: 'No code output.'
+        userFunction(request, response).catch(e => {
+            send(res, 500, {
+                code: 500,
+                message: e.code === 'MODULE_NOT_FOUND' ? "Code file not found." : e.stack || e
             });
-        }
+        });
     } catch (e) {
         send(res, 500, {
             code: 500,
-            message: e.code === 'MODULE_NOT_FOUND' ? 'Code file not found.' : e.stack || e
+            message: e.code === 'MODULE_NOT_FOUND' ? "Code file not found." : e.stack || e
         });
     }
 });
