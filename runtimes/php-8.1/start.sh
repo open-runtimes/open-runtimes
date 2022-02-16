9ed6c5d286196230477fb0a63513faadc8259950
Matej Bačo <matejbaco2000@gmail.com>
2022-02-16 21:46:11 +0000

    Example composer update

--- runtimes/php-8.1/start.sh
+++ runtimes/php-8.1/start.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-echo "OK1"
 cp /tmp/code.tar.gz /usr/workspace/code.tar.gz 
-echo "OK2"
 cd /usr/workspace/
-echo "OK3"
 tar -zxf /usr/workspace/code.tar.gz -C /usr/code-start
-echo "OK4"
 rm /usr/workspace/code.tar.gz
-echo "OK5"
 cp -r /usr/code-start/vendor /usr/local/src/vendor
-echo "OK6"
 cd /usr/local/src/
 php server.php
