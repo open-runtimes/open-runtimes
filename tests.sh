616498ae9460de17dd694a99b4189134eeb98a7d
Matej Bačo <matejbaco2000@gmail.com>
2022-02-17 18:05:12 +0000

    Fixed tests

--- tests.sh
+++ tests.sh
@@ -3,7 +3,7 @@ cd runtimes/${RUNTIME}
 docker build -t open-runtimes/test-runtime .
 cd ../../
 cd tests/${RUNTIME}
-docker run -d --name open-runtimes-test -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e ENTRYPOINT_NAME=${ENTRYPOINT} -e  INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "sh /usr/local/src/build.sh && cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
+docker run -d --name open-runtimes-test -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e ENTRYPOINT_NAME=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "sh /usr/local/src/build.sh && cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
 echo "Sleeping for 15 seconds, to make sure runtime has built and started"
 sleep 15
 docker ps -a
