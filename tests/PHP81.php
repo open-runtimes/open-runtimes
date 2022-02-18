dba8a208f5503ec783f6ffe1e3ac35e435b9b5b8
Matej Bačo <matejbaco2000@gmail.com>
2022-02-18 17:16:27 +0000

    Added tests for all versions

---
+++ tests/PHP81.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+// Runtime: php-8.1
+// PHP class: PHP81
+// Entrypoint: tests.php
+
+class PHP81 extends Base
+{
+}
---
+++ tests/Ruby30.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+// Runtime: ruby-3.0
+// PHP class: Ruby30
+// Entrypoint: tests.rb
+
+class Ruby30 extends Base
+{
+}
