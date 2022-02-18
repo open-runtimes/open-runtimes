17707c0d27eacc7191f0a2e7fd4779f7335e9f29
Jake Barnby <jakeb994@gmail.com>
2022-02-18 22:17:54 +1300

    Replace bash string contains

--- tests.sh
+++ tests.sh
@@ -5,11 +5,14 @@ cd ../../
 cd tests/${RUNTIME}
 docker run -d --name open-runtimes-test -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e ENTRYPOINT_NAME=${ENTRYPOINT} -e  INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "sh /usr/local/src/build.sh && cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
 echo "Sleeping to make sure runtime has built and started"
-if [[ "${RUNTIME}" == *"swift"* ]]; then
-  sleep 200
-else
-  sleep 15
-fi
+case "${RUNTIME}" in
+  *"swift"*)
+    sleep 200
+    ;;
+  *)
+    sleep 10
+    ;;
+esac
 docker ps -a
 cd ../../
 INTERNAL_RUNTIME_KEY=test-secret-key vendor/bin/phpunit tests/${PHP_CLASS}.php
