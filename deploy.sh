d5118fea2ad3877c91c4458e9ef04322b0952dd8
Eldad Fux <eldad.fux@gmail.com>
2022-02-18 22:09:50 +0200

    Updated architectures

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 echo 'Starting build...'
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le -t ${IMAGE} ./runtimes/${RUNTIME}/ --push
+docker buildx build --platform ${ARCH} -t ${IMAGE} ./runtimes/${RUNTIME}/ --push
