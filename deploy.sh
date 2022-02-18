a0d0a7a7ccf6b8d916d269140e87508835b989a5
Eldad Fux <eldad.fux@gmail.com>
2022-02-18 21:13:35 +0200

    Test AMD build

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
-echo 'Starting build...'
 
+echo 'Starting build test...'
+docker build -t ${IMAGE} ./runtimes/${RUNTIME}/ && docker push ${IMAGE}
+
+echo 'Starting build...'
 docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le -t ${IMAGE} ./runtimes/${RUNTIME}/ --push
