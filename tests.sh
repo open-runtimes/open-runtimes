b98a0ccd9be8277c7e271b0e4defde594fa6f87d
Jake Barnby <jakeb994@gmail.com>
2022-02-21 13:00:23 +1300

    Wait for swift server up before running tests

--- tests.sh
+++ tests.sh
@@ -3,14 +3,16 @@ cd runtimes/${RUNTIME}
 docker build -t open-runtimes/test-runtime .
 cd ../../
 cd tests/${RUNTIME}
-docker run -d --name open-runtimes-test -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e ENTRYPOINT_NAME=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "sh /usr/local/src/build.sh && cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
+id=$(docker run -d --name open-runtimes-test -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e ENTRYPOINT_NAME=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "sh /usr/local/src/build.sh && cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh")
 echo "Sleeping to make sure runtime has built and started"
 case "${RUNTIME}" in
   *"swift"*)
-    sleep 500
+    while [ -z $(docker container top $id | grep "serve") ]; do
+      sleep 10
+    done
     ;;
   *)
-    sleep 10
+    sleep 15
     ;;
 esac
 docker ps -a
