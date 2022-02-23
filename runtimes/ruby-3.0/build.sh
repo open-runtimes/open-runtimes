1a34b310a186bfba2978c269e498e5aad9597b64
Jake Barnby <jakeb994@gmail.com>
2022-02-23 17:25:14 +1300

    Fix ruby runtimes when build and start are run on separate container instances

--- runtimes/ruby-3.0/build.sh
+++ runtimes/ruby-3.0/build.sh
@@ -5,16 +5,21 @@ cp -R /usr/code/* /usr/builds
 
 # Append User Function Dependencies if Gemfile exists
 cd /usr/builds
-if [[ -f "Gemfile" ]]; then
+if [ -f "Gemfile" ]; then
+    EXISTING_GEMFILE=$(cat /usr/local/src/Gemfile)
     echo "eval_gemfile '/usr/builds/Gemfile'" >> /usr/local/src/Gemfile
 fi
 
-# Prepare server dependencies
+# Prepare dependencies
 cd /usr/local/src/
 bundle config set --local path 'vendor/bundle'
 bundle install
 
+if [ -f "Gemfile" ]; then
+    echo "${EXISTING_GEMFILE}" > Gemfile
+fi
+
 # Finish build by preparing tar to use for starting the runtime
 cd /usr/builds
-cp -R /usr/local/src/vendor /usr/builds/vendor
+cp -R /usr/local/src/vendor .
 tar --exclude code.tar.gz -zcf /usr/code/code.tar.gz .
