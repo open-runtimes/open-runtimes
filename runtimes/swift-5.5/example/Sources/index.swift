b25ce12502f088083b418ba86b94105df0277a8b
Matej Bačo <matejbaco2000@gmail.com>
2022-02-25 11:27:59 +0000

    Improved example docs

--- runtimes/swift-5.5/example/Sources/index.swift
+++ runtimes/swift-5.5/example/Sources/index.swift
@@ -8,6 +8,18 @@
 import Foundation
 import AsyncHTTPClient
 
+/*
+    'req' variable has:
+        'headers' - object with request headers
+        'payload' - object with request body data
+        'env' - object with environment variables
+    'res' variable has:
+        'send(text, status)' - function to return text response. Status code defaults to 200
+        'json(obj, status)' - function to return JSON response. Status code defaults to 200
+    
+    If an error is thrown, a response with code 500 will be returned.
+*/
+
 func main(req: RequestValue, res: RequestResponse) async throws -> RequestResponse {
     var todoId: String = "1"
 
