f097d99a0e0d7026263e696524e752668f250b63
Jake Barnby <jakeb994@gmail.com>
2022-02-25 13:36:17 +1300

    Update tests to use http instead of collections lib

--- tests/swift-5.5/Sources/Tests.swift
+++ tests/swift-5.5/Sources/Tests.swift
@@ -3,7 +3,7 @@ import Foundation
 import FoundationNetworking
 #endif
 
-import Collections
+import AsyncHTTPClient
 
 //    'req' variable has:
 //        'headers' - object with request headers
@@ -15,13 +15,12 @@ import Collections
 //    
 //    If an error is thrown, a response with code 500 will be returned.
 
-func main(req: RequestValue, res: RequestResponse) throws -> RequestResponse {
+func main(req: RequestValue, res: RequestResponse) async throws -> RequestResponse {
 
     let headerData = req.headers["x-test-header"]
     let envData = req.env["test-env"]
 
     var todoId: String = "1"
-    var todo: [String: Any]? = nil
 
     if !req.payload.isEmpty,
         let data = req.payload.data(using: .utf8),
@@ -29,16 +28,11 @@ func main(req: RequestValue, res: RequestResponse) throws -> RequestResponse {
             todoId = payload["id"] as! String
     }
 
-    let group = DispatchGroup()
-    let url = URL(string: "https://jsonplaceholder.typicode.com/todos/\(todoId)")!
-    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-        defer { group.leave() }
-        guard let data = data else { return }
-        todo = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-    }
-    group.enter()
-    task.resume()
-    group.wait()
+    let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+    let request = HTTPClientRequest(url: "https://jsonplaceholder.typicode.com/todos/\(todoId)")
+    let response = try await httpClient.execute(request, timeout: .seconds(30))
+    let data = try await response.body.collect(upTo: 1024 * 1024)
+    let todo = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
 
     return res.json(data: [
         "isTest": true,
