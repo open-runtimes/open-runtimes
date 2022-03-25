0a8af1d83b5679b9a143720be1d8ab3646438f14
Jake Barnby <jakeb994@gmail.com>
2022-03-25 20:28:47 +1300

    Add Kotlin 1.6 runtime

---
+++ runtimes/kotlin-1.6/src/main/kotlin/io/openruntimes/kotlin/Server.kt
@@ -0,0 +1,34 @@
+package io.openruntimes.kotlin
+
+import io.javalin.Javalin
+import io.javalin.http.Context
+import io.javalin.plugin.json.jsonMapper
+import io.openruntimes.kotlin.RuntimeResponse.Companion.error
+import io.openruntimes.kotlin.RuntimeResponse.Companion.unauthorized
+import kotlinx.coroutines.runBlocking
+
+suspend fun main() {
+    Javalin
+        .create()
+        .start(3000)
+        .post("/") { runBlocking { execute(it) } }
+}
+
+suspend fun execute(ctx: Context) {
+    RuntimeResponse.mapper = ctx.jsonMapper()
+
+    if (ctx.header("x-internal-challenge") != System.getenv("INTERNAL_RUNTIME_KEY")) {
+        ctx.json(unauthorized())
+        return
+    }
+
+    val codeWrapper = Wrapper()
+    val request = RuntimeRequest(ctx)
+    val response = RuntimeResponse()
+
+    try {
+        ctx.result(codeWrapper.main(request, response).data)
+    } catch (e: Exception) {
+        ctx.json(error(e))
+    }
+}
