61df15900be7dbca2e6c829e7e2edfb378f66c79
Matej Bačo <matejbaco2000@gmail.com>
2022-03-28 07:10:12 +0000

    PR review changes

--- runtimes/node-17.0/server.js
+++ runtimes/node-17.0/server.js
@@ -24,11 +24,15 @@ const server = micro(async (req, res) => {
     try {
         let userFunction = require(USER_CODE_PATH + '/' + process.env.INTERNAL_RUNTIME_ENTRYPOINT);
 
-        if (!(userFunction || userFunction.constructor || userFunction.call || userFunction.apply || userFunction.default)) {
+        if (!(userFunction || userFunction.constructor || userFunction.call || userFunction.apply)) {
             throw new Error("User function is not valid.")
         }
 
         if(userFunction.default) {
+            if (!(userFunction.default.constructor || userFunction.default.call || userFunction.default.apply)) {
+                throw new Error("User function is not valid.")
+            }
+            
             await userFunction.default(request, response);
         } else {
             await userFunction(request, response);
