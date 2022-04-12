924e474ddca3bbaf564f6739db125b8d403e361b
Jake Barnby <jakeb994@gmail.com>
2022-04-13 11:56:52 +1200

    Return unauthorised as string

--- runtimes/cpp-17/src/main.cc
+++ runtimes/cpp-17/src/main.cc
@@ -16,16 +16,17 @@ int main()
             [](const HttpRequestPtr &req,
                function<void(const HttpResponsePtr &)> &&callback, const RuntimeRequest &runtimeRequest)
             {
-                if (req->getHeader("x-internal-challenge") != getenv("INTERNAL_RUNTIME_KEY"))
+                const std::shared_ptr<HttpResponse> res = HttpResponse::newHttpResponse();
+
+                if (req->getHeader("x-internal-challenge") != std::getenv("INTERNAL_RUNTIME_KEY"))
                 {
-                    const shared_ptr<HttpResponse> resp = HttpResponse::newCustomHttpResponse(
-                        RuntimeResponse::unauthorized());
-                    callback(resp);
+                    httpResponse->setStatusCode(static_cast<HttpStatusCode>(500))
+                    httpResponse->setBody("Unauthorized");
+                    callback(httpResponse);
                     return;
                 }
 
                 RuntimeResponse &runtimeResponse = *new RuntimeResponse();
-                const shared_ptr<HttpResponse> httpResponse = HttpResponse::newHttpResponse();
 
                 try {
                     Wrapper::main(runtimeRequest, runtimeResponse);
