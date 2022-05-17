2277a45e119278a938a8f6883f4c99b4dbb195fe
Damodar Lohani <dlohani48@gmail.com>
2022-05-17 08:10:57 +0000

    java 18 runtime

---
+++ runtimes/java-18.0/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp /tmp/code.tar.gz /usr/workspace/code.tar.gz
+cd /usr/workspace
+tar -zxf /usr/workspace/code.tar.gz -C /usr/code
+rm /usr/workspace/code.tar.gz
+cd /usr/code
+java -jar java-runtime-1.0.0.jar
