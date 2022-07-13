3c16fc0e899235fd9cd5c4541712de78fbf6d2d1
Matej Bačo <matejbaco2000@gmail.com>
2022-07-13 16:34:36 +0000

    Improve caching in Deno 1.21

--- runtimes/deno-1.21/start.sh
+++ runtimes/deno-1.21/start.sh
@@ -3,6 +3,16 @@ cp /tmp/code.tar.gz /usr/workspace/code.tar.gz
 cd /usr/workspace 
 tar -zxf /usr/workspace/code.tar.gz -C /usr/code-start
 rm /usr/workspace/code.tar.gz
-export DENO_DIR="/usr/code-start/deno-cache"
 cd /usr/local/src/
-denon run --allow-net --allow-write --allow-read --allow-env server.ts
+
+# Prepare dependencies
+cp -R /usr/code-start/vendor .
+mkdir -p vendor
+
+# Run script, using import map if we have one
+FILE=vendor/import_map.json
+if [ -f "$FILE" ]; then
+    denon run --import-map=vendor/import_map.json --no-check --allow-net --allow-write --allow-read --allow-env server.ts
+else 
+    denon run --no-check --allow-net --allow-write --allow-read --allow-env server.ts
+fi
