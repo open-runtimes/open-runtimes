aa5b8000d5d52bae1c2a4cf72458b8831de6d160
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2022-07-19 13:35:52 +0545

    update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,14 +1,23 @@
+#!/bin/sh
+set -e
+cd runtimes/${RUNTIME}
+docker build -t open-runtimes/test-runtime .
+cd ../../
+cd tests/${RUNTIME}
+echo "Building..."
+touch code.tar.gz
+tar --exclude code.tar.gz -czf code.tar.gz .
 docker run --rm --name open-runtimes-test-build -v $(pwd)/code.tar.gz:/usr/code/code.tar.gz:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key open-runtimes/test-runtime sh -c "tar -xzf /usr/code/code.tar.gz -C /usr/code && sh /usr/local/src/build.sh"
 docker run --rm -d --name open-runtimes-test-serve -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
 echo "Waiting for server..."
 max_wait=500
 wait_interval=10
 wait_count=0
-while [ -z "$(docker container top open-runtimes-test-serve | grep "${SERVER_PROCESS}")" ]; do
-    [ $wait_count -gt $max_wait ] && echo "Server failed to start" && exit 1
-    wait_count=$((wait_count + wait_interval))
-    sleep $wait_interval
-done
+# while [ -z "$(docker container top open-runtimes-test-serve | grep "${SERVER_PROCESS}")" ]; do
+#     [ $wait_count -gt $max_wait ] && echo "Server failed to start" && exit 1
+#     wait_count=$((wait_count + wait_interval))
+#     sleep $wait_interval
+# done
 sleep $wait_interval
 cd ../../
 echo "Running tests..."
