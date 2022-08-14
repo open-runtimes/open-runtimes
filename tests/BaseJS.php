671e1d604f6f7de66c4cbdf03df032b020373acc
Damodar Lohani <dlohani48@gmail.com>
2022-08-14 10:09:33 +0000

    js test updated

--- tests/BaseJS.php
+++ tests/BaseJS.php
@@ -8,6 +8,7 @@ class BaseJS extends Base
 {
     public function testConsoleLogs(): void {
         $response = $this->call([]);
-        self::assertEquals("log\nwarning\nerror\ninfo\ndebug\nlog1 log2\n{\"hello\":\"world\"}\n[\"hello\",\"world\"]", $response['body']['stdout']);
+        self::assertEquals("log\ninfo\ndebug\nlog1 log2\n{\"hello\":\"world\"}\n[\"hello\",\"world\"]", $response['body']['stdout']);
+        self::assertEquals("warning\nerror", $response['body']['stderr']);
     }
 }
