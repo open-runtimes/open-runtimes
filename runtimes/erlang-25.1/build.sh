f1a7d78ee9907829920fa62f901b3544ecd1b6a0
ChandanChainani <chainanichan@gmail.com>
2022-11-08 21:00:36 +0530

    Updated build script

--- runtimes/erlang-25.1/build.sh
+++ runtimes/erlang-25.1/build.sh
@@ -2,44 +2,12 @@
 
 set -e
 
-mkdir -p /usr/local/src/src/main/java/io/openruntimes/java
-cp -a /usr/code/. /usr/local/src/src/main/java/io/openruntimes/java
+cp -a /usr/code/. /usr/local/src/server/src/
 
-cd /usr/local/src/src/main/java/io/openruntimes/java
+cd /usr/local/src/server
 
-# Apply all gradle files to the root project
-for filename in ./*.gradle*; do
-    if [ ! -f "${filename}" ]; then
-        continue;
-    fi
-    mv "${filename}" "/usr/local/src/${filename}"
-    echo "apply from: \"${filename}\"" >> /usr/local/src/build.gradle
-done
+# Build production release of erlang app
+rebar3 as prod release
 
-# Read user code and collect imports
-CODE="$(cat "$INTERNAL_RUNTIME_ENTRYPOINT")"
-IMPORTS=""
-while read line; do
-    case "${line}" in import*)
-        IMPORTS="${IMPORTS}${line}\n"
-    esac
-done < "$INTERNAL_RUNTIME_ENTRYPOINT"
-CODE=$(echo "${CODE}" | sed /import*/d)
-
-# Wrap the user code in a class
-echo "package io.openruntimes.java;
-${IMPORTS}
-public class Wrapper {
-${CODE}
-}" > Wrapper.java
-
-# Remove the user code file (copy)
-rm "${INTERNAL_RUNTIME_ENTRYPOINT}"
-
-# Build the jar
-cd /usr/local/src
-sh gradlew buildJar
-
-# Tar the jar
-cd /usr/local/src/build/libs/
-tar -zcvf /usr/code/code.tar.gz .
+# Tar the app
+tar -zcvf /usr/code/code.tar.gz /usr/local/src/server/_build/prod/rel/server/
