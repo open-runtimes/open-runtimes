2c9c01be402dd6514e44f7e898be120537079b9f
Damodar Lohani <dlohani48@gmail.com>
2022-11-16 09:30:43 +0000

    node 19 runtime source

---
+++ runtimes/node-19.0/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Fail build if any command fails
+set -e
+
+# Prepare separate directory to prevent changing user's files
+cp -R /usr/code/* /usr/builds
+
+# Install User Function Dependencies if package.json exists
+cd /usr/builds
+if [ -f package.json ]; then
+  npm install
+fi
+
+mkdir -p node_modules
+
+# Merge the node_modules from the server into the user's node_modules to be restored later.
+cp -R /usr/local/src/node_modules/* /usr/builds/node_modules
+
+# Finish build by preparing tar to use for starting the runtime
+tar --exclude code.tar.gz -zcf /usr/code/code.tar.gz .
