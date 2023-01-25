b0390f6ffafb4318bdc4c491b659a91076c8a9a6
Matej Bačo <matejbaco2000@gmail.com>
2023-01-25 09:14:34 +0000

    Fix test script after folder renaming

--- tests-v3.sh
+++ tests-v3.sh
@@ -7,7 +7,7 @@ cd tests/${RUNTIME}
 echo "Building..."
 touch code.tar.gz
 mkdir -p resources
-cp -r ../_resources/* resources
+cp -r ../resources/* resources
 tar --exclude code.tar.gz -czf code.tar.gz .
 docker run --rm --name open-runtimes-test-build -v $(pwd)/code.tar.gz:/usr/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} -e OPEN_RUNTIMES_SECRET=test-secret-key open-runtimes/test-runtime sh -c "tar -xzf /usr/code/code.tar.gz -C /usr/code && sh /usr/local/src/build.sh"
 docker run --rm -d --name open-runtimes-test-serve -v $(pwd):/usr/code:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} -e OPEN_RUNTIMES_SECRET=test-secret-key -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
