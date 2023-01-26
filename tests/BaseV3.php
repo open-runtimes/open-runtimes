3529464109073b3ddcc2c55e30bc6550869e855d
Matej Bačo <matejbaco2000@gmail.com>
2023-01-26 18:09:49 +0000

    Fix tests

--- tests/BaseV3.php
+++ tests/BaseV3.php
@@ -170,10 +170,6 @@ abstract class BaseV3 extends TestCase
         self::assertEquals(200, $response['code']);
         self::assertEquals('DELETE', $response['body']);
 
-        $response = $this->execute(method: 'CONNECT', headers: ['x-action' => 'requestMethod']);
-        self::assertEquals(200, $response['code']);
-        self::assertEquals('CONNECT', $response['body']);
-
         $response = $this->execute(method: 'OPTIONS', headers: ['x-action' => 'requestMethod']);
         self::assertEquals(200, $response['code']);
         self::assertEquals('OPTIONS', $response['body']);
