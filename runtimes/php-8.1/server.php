e01f5276cd63b4013207f1c30347b15382f7be3d
Matej Bačo <matejbaco2000@gmail.com>
2023-01-27 10:33:40 +0000

    Implement PHP v3

--- runtimes/php-8.1/server.php
+++ runtimes/php-8.1/server.php
@@ -3,96 +3,138 @@ $server = new Swoole\HTTP\Server("0.0.0.0", 3000);
 
 const USER_CODE_PATH = '/usr/code-start';
 
-function join_paths() {
-    $paths = array();
-
-    foreach (func_get_args() as $arg) {
-        if ($arg !== '') { $paths[] = $arg; }
+class Response {
+    function send(string $body, int $statusCode = 200, array $headers = []): array {
+        return [
+            'body' => $body,
+            'statusCode' => $statusCode,
+            'headers' => $headers,
+        ];
     }
 
-    return preg_replace('#/+#','/',join('/', $paths));
+    function json(array $obj, int $statusCode = 200, array $headers = []) {
+        $headers['content-type'] = 'application/json';
+        return $this->send(\json_encode($obj), $statusCode, $headers);
+    }
+
+    function empty() {
+        return $this->send('', 204, []);
+    }
+
+    function redirect(string $url, int $statusCode = 301, array $headers = []) {
+        $headers['location'] = $url;
+        return $this->send('', $statusCode, $headers);
+    }
 }
 
-class Response {
-    private mixed $response;
-    private int $status = 200;
-    private array $headers = [];
+class Request {
+    public string $rawBody = '';
+    public mixed $body = '';
+    public array $headers = [];
+    public string $method = '';
+    public string $url = '';
+}
 
-    function getStatus(): int {
-        return $this->status;
+class Context {
+    public Request $req;
+    public Response $res;
+
+    public array $_logs = [];
+    public array $_errors = [];
+
+    function __construct() {
+        $this->req = new Request();
+        $this->res = new Response();
     }
 
-    function getResponse(): mixed  {
-        return $this->response;
+    // TODO: Support many params
+    function log(mixed $message) {
+        $this->_logs[] = \strval($message);
     }
 
-    function getHeaders(): array {
-        return $this->headers;
-    }
-
-    function send($text, $status = 200) {
-        $this->response = $text;
-        $this->status = $status;
-    }
-
-    function json($json, $status = 200) {
-        $this->status = $status;
-        $this->response = $json;
-        $this->headers['Content-Type'] = 'application/json';
+    function error(mixed $message) {
+        $this->_errors[] = \strval($message);
     }
 }
 
 $userFunction = null;
 
 $server->on("Request", function($req, $res) use(&$userFunction) {
-    $body =  json_decode($req->getContent(), true);
-    $body['payload'] = \is_string($body['payload']) ? $body['payload'] : \json_encode($body['payload'], JSON_FORCE_OBJECT);
-
-    // Get internal_challenge header
-    $internal_challenge = $req->header['x-internal-challenge'];
-
-    if (empty($internal_challenge)) {
+    if (empty($req->header['x-open-runtimes-secret']) || $req->header['x-open-runtimes-secret'] !== getenv('OPEN_RUNTIMES_SECRET')) {
         $res->status(500);
-        $res->end(json_encode(['stderr' => 'Unauthorized']));
+        $res->end('Unauthorized. Provide correct "x-open-runtimes-secret" header.');
         return;
     }
 
-    $key = getenv('INTERNAL_RUNTIME_KEY');
+    $context = new Context();
 
-    if ($key != $internal_challenge) {
-        $res->status(500);
-        $res->end(json_encode(['stderr' => 'Unauthorized']));
-        return;
+    $url = $req->server['path_info'];
+
+    if(!empty($req->server['query_string'])) {
+        $url .= '?' . $req->server['query_string'];
     }
 
-    $request = [
-        'variables' => $body['variables'] ?? [],
-        'headers' => $body['headers'] ?? [],
-        'payload' => $body['payload'] ?? ''
-    ];
+    $context->req->rawBody = $req->getContent();
+    $context->req->body = $context->req->rawBody;
+    $context->req->method = $req->getMethod();
+    $context->req->url = $url;
+    $context->req->headers = [];
 
-    $response = new Response($res);
+    $contentType = $req->header['content-type'] ?? 'text/plain';
+    if(\str_contains($contentType, 'application/json')) {
+        $context->req->body = json_decode($context->req->rawBody, true);
+    }
 
+    foreach ($req->header as $header => $value) {
+        if(!(\str_starts_with(\strtolower($header), 'x-open-runtimes-'))) {
+            $context->req->headers[\strtolower($header)] = $value;
+        }
+    }
+
+    $customstd = null;
+
+    $output = null;
     try {
         if($userFunction === null) {
-            $userFunction = include(USER_CODE_PATH . '/' . getenv('INTERNAL_RUNTIME_ENTRYPOINT'));
+            $userFunction = include(USER_CODE_PATH . '/' . getenv('OPEN_RUNTIMES_ENTRYPOINT'));
         }
 
         if (!is_callable($userFunction)) {
-            return throw new Exception('Function not valid');
+            throw new Exception('User function is not valid.');
         }
-        ob_start();
-        $userFunction($request, $response);
-        $stdout = ob_get_clean();
 
-        $res->status($response->getStatus());
-        $res->header = $response->getHeaders();
-        $res->end(json_encode(['response' => $response->getResponse(), 'stdout' => $stdout], JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
-        $stdout = '';
+        ob_start();
+        $output = $userFunction($context);
+        $customstd = ob_get_clean();
     } catch (\Throwable $e) {
-        $res->status(500);
-        return $res->end(json_encode(['stderr' => $e->getMessage()."\r\n".$e->getTraceAsString()], JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
+        $context->error($e->getMessage()."\n".$e->getTraceAsString());
+        $output = $context->res->send('', 500, []);
     }
+
+    if($output == null) {
+        $context->error('Return statement missing. return context.res.empty() if no response is expected.');
+        $output = $context->res->send('', 500, []);
+    }
+
+    $output['body'] ??= '';
+    $output['statusCode'] ??= 200;
+    $output['headers'] ??= [];
+
+    foreach ($output['headers'] as $header => $value) {
+        if(!(\str_starts_with(\strtolower($header), 'x-open-runtimes-'))) {
+            $res->header(\strtolower($header), $value);
+        }
+    }
+
+    if(!empty($customstd)) {
+        $context->log('Unsupported log noticed. Use context.log() or context.error() for logging.');
+    }
+
+    $res->header('x-open-runtimes-logs', \urlencode(\implode('\n', $context->_logs)));
+    $res->header('x-open-runtimes-errors', \urlencode(\implode('\n', $context->_errors)));
+
+    $res->status($output['statusCode']);
+    $res->end($output['body']);
 });
 
 $server->start();
