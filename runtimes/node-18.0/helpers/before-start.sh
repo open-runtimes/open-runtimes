0b2efed15f6a2828fe4b4fc27b6325ba3e6a0a5d
Matej Bačo <matejbaco2000@gmail.com>
2023-03-01 13:04:36 +0000

    Update function folder during runtime

--- runtimes/node-18.0/helpers/before-start.sh
+++ runtimes/node-18.0/helpers/before-start.sh
@@ -2,19 +2,19 @@
 echo "Preparing for start ..."
 
 # Extract gzipped code from mounted volume to function folder
-tar -zxf /mnt/code/code.tar.gz -C /usr/local/server/function
+tar -zxf /mnt/code/code.tar.gz -C /usr/local/server/src/function
 
 # Apply env vars from build step
 set -o allexport
-source /usr/local/server/function/.open-runtimes
+source /usr/local/server/src/function/.open-runtimes
 set +o allexport
 
 # Prepare empty node_modules to prevent errors with copying
-mkdir -p /usr/local/server/function/node_modules
+mkdir -p /usr/local/server/src/function/node_modules
 
 # Merge user's node_modules into server's node_modules
 setopt no_nomatch
-cp -R /usr/local/server/function/node_modules/* /usr/local/server/node_modules
+cp -R /usr/local/server/src/function/node_modules/* /usr/local/server/node_modules
 setopt nomatch
 
 # Enter server folder
