962518ba349b0eddad81278ba2c6292f7d096921
Matej Bačo <matejbaco2000@gmail.com>
2023-03-05 10:52:36 +0000

    Fix node tests

--- runtimes/node-18.0/helpers/after-build.sh
+++ runtimes/node-18.0/helpers/after-build.sh
@@ -4,6 +4,12 @@ set -e
 
 echo "Packing build ..."
 
+# Prepare empty node_modules to prevent errors with copying
+mkdir -p /usr/local/build/node_modules
+
+# Merge user's node_modules into server's node_modules
+cp -R /usr/local/server/node_modules/. /usr/local/build/node_modules
+
 # Store entrypoint into build. Will be used during start process
 touch /usr/local/build/.open-runtimes
 echo "OPEN_RUNTIMES_ENTRYPOINT=$OPEN_RUNTIMES_ENTRYPOINT" > /usr/local/build/.open-runtimes
--- runtimes/node-18.0/helpers/before-start.sh
+++ runtimes/node-18.0/helpers/before-start.sh
@@ -12,14 +12,6 @@ set -o allexport
 source /usr/local/server/src/function/.open-runtimes
 set +o allexport
 
-# Prepare empty node_modules to prevent errors with copying
-mkdir -p /usr/local/server/src/function/node_modules
-
-# Merge user's node_modules into server's node_modules
-setopt no_nomatch
-cp -R /usr/local/server/src/function/node_modules/* /usr/local/server/node_modules
-setopt nomatch
-
 # Enter server folder
 cd /usr/local/server
 
