c238667bfba67f7e38da0f64d36f79546911d950
Matej Bačo <matejbaco2000@gmail.com>
2023-05-03 12:27:14 +0200

    Add version to test

--- tests.sh
+++ tests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 cd runtimes/${RUNTIME}
-docker buildx build -t open-runtimes/test-runtime .
+docker buildx build -t open-runtimes/test-runtime:0.1 .
 cd ../../
 cd tests/${RUNTIME}
 echo "Building..."
 touch code.tar.gz
 tar --exclude code.tar.gz -czf code.tar.gz .
-docker run --rm --name open-runtimes-test-build -v $(pwd)/code.tar.gz:/usr/code/code.tar.gz:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key open-runtimes/test-runtime sh -c "tar -xzf /usr/code/code.tar.gz -C /usr/code && sh /usr/local/src/build.sh"
-docker run --rm -d --name open-runtimes-test-serve -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime sh -c "cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
+docker run --rm --name open-runtimes-test-build -v $(pwd)/code.tar.gz:/usr/code/code.tar.gz:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key open-runtimes/test-runtime:0.1 sh -c "tar -xzf /usr/code/code.tar.gz -C /usr/code && sh /usr/local/src/build.sh"
+docker run --rm -d --name open-runtimes-test-serve -v $(pwd):/usr/code:rw -e INTERNAL_RUNTIME_ENTRYPOINT=${ENTRYPOINT} -e INTERNAL_RUNTIME_KEY=test-secret-key -p 3000:3000 open-runtimes/test-runtime:0.1 sh -c "cp /usr/code/code.tar.gz /tmp/code.tar.gz && sh /usr/local/src/start.sh"
 echo "Waiting for server..."
 max_wait=500
 wait_interval=10
