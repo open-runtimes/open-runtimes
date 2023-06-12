bd3c731c1c29c94fa0d3be67c2245cc4338dc719
Andreas Loizides <andreas16700@icloud.com>
2023-06-12 19:39:46 +0300

    Add tests

---
+++ tests/Swift58.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+// Runtime: swift-5.8
+// PHP class: Swift58
+// Entrypoint: Tests.swift
+
+class Swift58 extends Base
+{
+    public function testConsoleLogs(): void {
+        return; // Swift doesn't yet support console log.
+    }
+}
---
+++ tests/swift-5.8/Sources/Tests.swift
@@ -0,0 +1,55 @@
+import Foundation
+import AsyncHTTPClient
+
+//    'req' variable has:
+//        'headers' - object with request headers
+//        'payload' - object with request body data
+//        'variables' - object with function variables
+//    'res' variable has:
+//        'send(text, status)' - function to return text response. Status code defaults to 200
+//        'json(obj, status)' - function to return JSON response. Status code defaults to 200
+//    
+//    If an error is thrown, a response with code 500 will be returned.
+
+func main(req: RequestValue, res: RequestResponse) async throws -> RequestResponse {
+
+    let headerData = req.headers["x-test-header"]
+    let varData = req.variables["test-variable"]
+
+    var todoId: String = "1"
+
+    var reqPayload = req.payload as! String
+    if(reqPayload == "") {
+        reqPayload = "{}"
+    }
+
+    if !reqPayload.isEmpty,
+        let data = reqPayload.data(using: .utf8),
+        let payload = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            todoId = payload["id"] as? String ?? "1"
+    }
+
+    let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+    let request = HTTPClientRequest(url: "https://jsonplaceholder.typicode.com/todos/\(todoId)")
+    let response = try await httpClient.execute(request, timeout: .seconds(30))
+    let data = try await response.body.collect(upTo: 1024 * 1024)
+    let todo = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+
+    print("String1")
+    print(42)
+    print(4.2)
+    print(true)
+
+    print("String2")
+    print("String3")
+    print("String4")
+    print("String5")
+
+    return res.json(data: [
+        "isTest": true,
+        "message": "Hello Open Runtimes 👋",
+        "todo": todo,
+        "header": headerData,
+        "variable": varData
+    ])
+}
