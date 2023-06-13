65642598ad103c1df04a33037e2b957b01611812
Jake Barnby <jakeb994@gmail.com>
2023-06-14 00:34:46 +1200

    Dotnet safe timeouts

--- tests/resources/functions/dotnet-6.0/Tests.cs
+++ tests/resources/functions/dotnet-6.0/Tests.cs
@@ -7,120 +7,127 @@ using System.Text.Json;
 public class Handler {
     static readonly HttpClient http = new();
 
-    public async Task<RuntimeOutput> Main(RuntimeContext Context)
+    public async Task<RuntimeOutput> Main(RuntimeContext context)
     {
-        var Action = Context.Req.Headers.TryGetValue("x-action", out string ActionValue) ? ActionValue : "";
+        var Action = context.Req.Headers.TryGetValue("x-action", out string ActionValue) ? ActionValue : "";
 
         switch (Action)
         {
             case "plaintextResponse":
-                return Context.Res.Send("Hello World 👋");
+                return context.Res.Send("Hello World 👋");
             case "jsonResponse":
-                return Context.Res.Json(new()
+                return context.Res.Json(new()
                 {
                     { "json", true },
                     { "message", "Developers are awesome." }
                 });
             case "redirectResponse":
-                return Context.Res.Redirect("https://github.com/");
+                return context.Res.Redirect("https://github.com/");
             case "emptyResponse":
-                return Context.Res.Empty();
+                return context.Res.Empty();
             case "noResponse":
-                Context.Res.Send("This should be ignored, as it is not returned.");
+                context.Res.Send("This should be ignored, as it is not returned.");
 
                 // Simulate test data. Return nessessary in Java
-                Context.Error("Return statement missing. return Context.Res.Empty() if no response is expected.");
-                return Context.Res.Send("", 500);
+                context.Error("Return statement missing. return context.Res.Empty() if no response is expected.");
+                return context.Res.Send("", 500);
             case "doubleResponse":
-                Context.Res.Send("This should be ignored.");
-                return Context.Res.Send("This should be returned.");
+                context.Res.Send("This should be ignored.");
+                return context.Res.Send("This should be returned.");
             case "headersResponse":
-                var Headers = new Dictionary<string, string>();
-                Headers.Add("first-header", "first-value");
-                var SecondHeader = Context.Req.Headers.TryGetValue("x-open-runtimes-custom-in-header", out string SecondHeaderValue) ? SecondHeaderValue : "missing";
-                Headers.Add("second-header", SecondHeader);
-                Headers.Add("x-open-runtimes-custom-out-header", "third-value");
-                return Context.Res.Send("OK", 200, Headers);
+                var headers = new Dictionary<string, string>();
+                headers.Add("first-header", "first-value");
+                var SecondHeader = context.Req.Headers.TryGetValue("x-open-runtimes-custom-in-header", out string secondHeaderValue) ? secondHeaderValue : "missing";
+                headers.Add("second-header", SecondHeader);
+                headers.Add("x-open-runtimes-custom-out-header", "third-value");
+                return context.Res.Send("OK", 200, headers);
             case "statusResponse":
-                return Context.Res.Send("FAIL", 404);
+                return context.Res.Send("FAIL", 404);
             case "requestMethod":
-                return Context.Res.Send(Context.Req.Method);
+                return context.Res.Send(context.Req.Method);
             case "requestUrl":
-                return Context.Res.Json(new()
+                return context.Res.Json(new()
                 {
-                    { "url", Context.Req.Url },
-                    { "port", Context.Req.Port },
-                    { "path", Context.Req.Path },
-                    { "query", Context.Req.Query },
-                    { "queryString", Context.Req.QueryString },
-                    { "scheme", Context.Req.Scheme },
-                    { "host", Context.Req.Host }
+                    { "url", context.Req.Url },
+                    { "port", context.Req.Port },
+                    { "path", context.Req.Path },
+                    { "query", context.Req.Query },
+                    { "queryString", context.Req.QueryString },
+                    { "scheme", context.Req.Scheme },
+                    { "host", context.Req.Host }
                 });
             case "requestHeaders":
-                var Json = new Dictionary<string, object>();
+                var json = new Dictionary<string, object>();
 
-                foreach (var Entry in Context.Req.Headers)
+                foreach (var entry in context.Req.Headers)
                 {
-                    Json.Add(Entry.Key, Entry.Value);
+                    json.Add(entry.Key, entry.Value);
                 }
 
-                return Context.Res.Json(Json);
+                return context.Res.Json(json);
             case "requestBodyPlaintext":
-                return Context.Res.Send((string) Context.Req.Body);
+                return context.Res.Send((string) context.Req.Body);
             case "requestBodyJson":
-                var Key1 = "";
-                var Key2 = "";
+                var key1 = "";
+                var key2 = "";
 
-                if(Context.Req.Body is string) {
-                    Key1 = "Missing key";
-                    Key2 = "Missing key";
+                if(context.Req.Body is string) {
+                    key1 = "Missing key";
+                    key2 = "Missing key";
                 } else {
-                    var Body = (Dictionary<String, Object>) Context.Req.Body;
+                    var body = (Dictionary<String, Object>) context.Req.Body;
 
-                    Key1 = Body.TryGetValue("key1", out var Key1Value) ? Key1Value.ToString() : "Missing key";
-                    Key2 = Body.TryGetValue("key2", out var Key2Value) ? Key2Value.ToString() : "Missing key";
+                    key1 = body.TryGetValue("key1", out var key1Value) ? key1Value.ToString() : "Missing key";
+                    key2 = body.TryGetValue("key2", out var key2Value) ? key2Value.ToString() : "Missing key";
                 }
 
-                return Context.Res.Json(new()
+                return context.Res.Json(new()
                 {
-                    { "key1", Key1 },
-                    { "key2", Key2 },
-                    { "raw", Context.Req.BodyString }
+                    { "key1", key1 },
+                    { "key2", key2 },
+                    { "raw", context.Req.BodyString }
                 });
             case "envVars":
-                return Context.Res.Json(new()
+                return context.Res.Json(new()
                 {
                     { "var", Environment.GetEnvironmentVariable("CUSTOM_ENV_VAR") ?? null },
                     { "emptyVar", Environment.GetEnvironmentVariable("NOT_DEFINED_VAR") ?? null },
                 });
             case "logs":
                 Console.WriteLine("Native log");
-                Context.Log("Debug log");
-                Context.Error("Error log");
+                context.Log("Debug log");
+                context.Error("Error log");
                 
-                Context.Log(42);
-                Context.Log(4.2);
-                Context.Log(true);
+                context.Log(42);
+                context.Log(4.2);
+                context.Log(true);
 
                 var Obj = new Dictionary<string, string>();
                 Obj.Add("objectKey", "objectValue");
 
-                Context.Log(Obj);
+                context.Log(Obj);
 
                 var Arr = new List<string>();
                 Arr.Add("arrayValue");
 
-                Context.Log(Arr);
+                context.Log(Arr);
 
-                return Context.Res.Send("");
+                return context.Res.Send("");
             case "library":
-                var response = await http.GetStringAsync($"https://jsonplaceholder.typicode.com/todos/{Context.Req.BodyString}");
+                var response = await http.GetStringAsync($"https://jsonplaceholder.typicode.com/todos/{context.Req.BodyString}");
                 var todo = JsonSerializer.Deserialize<Dictionary<string, object>>(response) ?? new Dictionary<string, object>();
 
-                return Context.Res.Json(new()
+                return context.Res.Json(new()
                 {
                     { "todo", todo }
                 });
+            case "timeout":
+                context.Log("Timeout start.");
+
+                await Task.Delay(3000);
+
+                context.Log("Timeout end.");
+                return context.Res.Send("Successful response.");
             default:
                 throw new Exception("Unknown action");
         }
