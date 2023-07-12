da359f99c1795e95e992fbe05edf0d57a2616dea
Matej Bačo <matejbaco2000@gmail.com>
2023-07-12 10:10:45 +0000

    Fix tests

--- tests/Base.php
+++ tests/Base.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-abstract class Base extends TestCase
+class Base extends TestCase
 {
     public function setUp(): void
     {
