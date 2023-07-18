0798ef88e16793d9b737d14c2e0565881a189e9d
Matej Bačo <matejbaco2000@gmail.com>
2023-07-18 07:02:41 +0000

    Add OPR prefix to env vars

--- tests.sh
+++ tests.sh
@@ -13,8 +13,8 @@ rm -rf code.tar.gz
 touch code.tar.gz
 
 # Build and start runtime
-docker run --rm --name open-runtimes-test-build -v $(pwd):/mnt/code:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} open-runtimes/test-runtime sh -c "sh helpers/build.sh \"$BUILD_COMMAND\""
-docker run -d --rm --name open-runtimes-test-serve -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} -e OPEN_RUNTIMES_SECRET=test-secret-key -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$START_COMMAND\""
+docker run --rm --name open-runtimes-test-build -v $(pwd):/mnt/code:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} open-runtimes/test-runtime sh -c "sh helpers/build.sh \"$OPEN_RUNTIMES_BUILD_COMMAND\""
+docker run -d --rm --name open-runtimes-test-serve -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} -e OPEN_RUNTIMES_SECRET=test-secret-key -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$OPEN_RUNTIMES_START_COMMAND\""
 
 # Wait for runtime to start
 echo "Waiting for servers..."
