231e5254ed97d5cad28bee1f6742c34a8b0eda13
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2023-07-19 16:02:05 -0400

    [Node] - Adapting servers to pass the SIGINT to the child server.

--- runtimes/node-14.5/start.sh
+++ runtimes/node-14.5/start.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-cp /tmp/code.tar.gz /usr/workspace/code.tar.gz 
+cp /tmp/code.tar.gz /usr/workspace/code.tar.gz
 cd /usr/workspace
 tar -zxf /usr/workspace/code.tar.gz -C /usr/code-start
 rm /usr/workspace/code.tar.gz
 cp -R /usr/code-start/node_modules/* /usr/local/src/node_modules
 cd /usr/local/src
-npm start
+
+close() {
+  kill -s SIGINT $(pgrep -f "node /usr/local/src/server.js")
+  sleep 3
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap close SIGTERM
+
+npm start &
+
+child=$!
+wait "$child"
