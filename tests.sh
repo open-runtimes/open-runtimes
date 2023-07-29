a4416420c5dbdfa92157977df5b5e18aebddaba9
faisalill <mfaisal.ilyas.d@gmail.com>
2023-07-29 19:46:36 +0530

    Fix travis conflicting names

--- tests.sh
+++ tests.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
 # Build runtime Docker image
-cd runtimes/$RUNTIME
-# docker build --load -t open-runtimes/test-runtime .
-docker build -t open-runtimes/test-runtime .
-docker save -o image.tar open-runtimes/test-runtime
-docker load -i image.tar
+cd runtimes/${RUNTIME}
+docker build --load -t open-runtimes/test-runtime .
 
 # Enter function folder
 cd ../../
-cd tests/resources/functions/$RUNTIME
+cd tests/resources/functions/${RUNTIME}
 
 # Prevent Docker mount from creating directory
 rm -rf code.tar.gz
 touch code.tar.gz
 
 # Build and start runtime
-docker run --rm --name open-runtimes-test-build -v $(pwd):/mnt/code:rw -e OPEN_RUNTIMES_ENTRYPOINT=$ENTRYPOINT open-runtimes/test-runtime sh -c "sh helpers/build.sh \"$OPEN_RUNTIMES_BUILD_COMMAND\""
-docker run -d --rm --name open-runtimes-test-serve -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT=$ENTRYPOINT -e OPEN_RUNTIMES_SECRET=test-secret-key -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$OPEN_RUNTIMES_START_COMMAND\""
+docker run --rm --name open-runtimes-test-build -v $(pwd):/mnt/code:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} open-runtimes/test-runtime sh -c "sh helpers/build.sh \"$OPEN_RUNTIMES_BUILD_COMMAND\""
+docker run -d --rm --name open-runtimes-test-serve -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} -e OPEN_RUNTIMES_SECRET=test-secret-key -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$OPEN_RUNTIMES_START_COMMAND\""
 
 # Wait for runtime to start
 echo "Waiting for servers..."
@@ -26,4 +23,4 @@ sleep 10
 # Run tests
 cd ../../../../
 echo "Running tests..."
-OPEN_RUNTIMES_SECRET=test-secret-key OPEN_RUNTIMES_ENTRYPOINT=$ENTRYPOINT vendor/bin/phpunit --configuration phpunit.xml tests/$TEST_CLASS.php
+OPEN_RUNTIMES_SECRET=test-secret-key OPEN_RUNTIMES_ENTRYPOINT=${ENTRYPOINT} vendor/bin/phpunit --configuration phpunit.xml tests/${TEST_CLASS}.php
