39310a9abf35fe53f6c781168c0736409e58bf23
faisalill <mfaisal.ilyas.d@gmail.com>
2023-08-01 16:59:35 +0530

    Remove travis files

--- .github/workflows/deploy.sh
+++
@@ -1,8 +0,0 @@
-echo 'Starting deployment...'
-
-if [ -z "$ARCH" ]
-then
-    echo "ARCH missing, skipping deployment"
-else
-    docker buildx build --platform $ARCH -t $IMAGE ./runtimes/$RUNTIME/ --push
-fi
