bef835d68a4b5e66023d4a57aa20a85938e63aaa
loks0n <22452787+loks0n@users.noreply.github.com>
2023-08-28 16:56:21 +0100

    fix: cpp

--- runtimes/cpp-20/src/main.cc
+++ runtimes/cpp-20/src/main.cc
@@ -280,13 +280,18 @@ int main()
                 }
                 
                 std::string contentTypeHeader = res->getHeader("content-type");
-                if (!contentTypeHeader.empty() && 
-                    contentTypeHeader.find("multipart/") == std::string::npos &&
+                if (contentTypeHeader.empty())
+                {
+                    contentTypeHeader = "text/plain";
+                }
+
+                if (contentTypeHeader.find("multipart/") == std::string::npos &&
                     contentTypeHeader.find("charset=") == std::string::npos)
                 {
                     contentTypeHeader.append("; charset=utf-8");
-                    res->addHeader("content-type", contentTypeHeader);
                 }
+
+                res->addHeader("content-type", contentTypeHeader);
 
                 CURL *curl = curl_easy_init();
 
