57839ac2f2b61d2823b721d4b256bbbd9437129f
Aditya Oberai <adityaoberai1@gmail.com>
2023-11-16 07:09:39 +0000

    Add dotnet-8.0 runtime

---
+++ runtimes/dotnet-8.0/src/RuntimeRequest.cs
@@ -0,0 +1,44 @@
+namespace DotNetRuntime
+{
+	public class RuntimeRequest
+	{
+		public string Method { get; private set; }
+		public string Scheme { get; private set; }
+		public string Host { get; private set; }
+		public int Port { get; private set; }
+		public string Path { get; private set; }
+		public Dictionary<string, string> Query { get; private set; }
+		public string QueryString { get; private set; }
+		public string Url { get; private set; }
+		public Dictionary<string, string> Headers { get; private set; }
+		public object Body { get; private set; }
+		public string BodyRaw { get; private set; }
+
+		public RuntimeRequest(
+		    string method,
+		    string scheme,
+		    string host,
+		    int port,
+		    string path,
+		    Dictionary<string, string> query,
+		    string queryString,
+		    string url,
+		    Dictionary<string, string> headers,
+		    object body,
+		    string bodyRaw)
+		{
+			BodyRaw = bodyRaw;
+			Body = body;
+			Headers = headers;
+			Method = method;
+			Url = url;
+			Host = host;
+			Scheme = scheme;
+			Path = path;
+			QueryString = queryString;
+			Query = query;
+			Port = port;
+		}
+	}
+}
+
