116eaa7be2bc53ea877b649e29d0ddb3da05383e
loks0n <22452787+loks0n@users.noreply.github.com>
2024-02-29 14:21:46 +0000

    chore: bump async-http-client

--- tests/resources/functions/swift-5.9/Package.swift
+++ tests/resources/functions/swift-5.9/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "swift-runtime",
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.20.1"),
     ],
     targets: [
         .executableTarget(
