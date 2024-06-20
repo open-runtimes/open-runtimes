06534caa4e6153fd4ab886606dee284bb326641a
Matej Bačo <matejbaco2000@gmail.com>
2024-06-20 10:31:42 +0000

    Binaries in all C++ versions

--- runtimes/cpp-17/src/RuntimeResponse.h
+++ runtimes/cpp-17/src/RuntimeResponse.h
@@ -10,30 +10,46 @@ namespace runtime
     class RuntimeResponse
     {
         public:
-            RuntimeOutput send(const std::string &body, const int statusCode = 200, const Json::Value &headers = {})
+            RuntimeOutput binary(const std::vector<std::byte> &bytes, const int statusCode = 200, const Json::Value &headers = {})
             {
                 RuntimeOutput output;
-                output.body = body;
+                output.body = bytes;
                 output.statusCode = statusCode;
                 output.headers = headers;
                 return output;
             }
 
+            RuntimeOutput send(const std::string &body, const int statusCode = 200, const Json::Value &headers = {})
+            {
+                return this->text(body, statusCode, headers);
+            }
+
+            RuntimeOutput text(const std::string &body, const int statusCode = 200, const Json::Value &headers = {})
+            {
+                std::vector<std::byte> bytes;
+                bytes.reserve(body.size());
+                std::transform(std::begin(body), std::end(body), std::back_inserter(bytes), [](char c){
+                    return std::byte(c);
+                });
+
+                return this->binary(bytes, statusCode, headers);
+            }
+
             RuntimeOutput json(const Json::Value &json, const int statusCode = 200, Json::Value headers = {})
             {
                 headers["content-type"] = "application/json";
-                return this->send(json.toStyledString(), statusCode, headers);
+                return this->text(json.toStyledString(), statusCode, headers);
             }
 
             RuntimeOutput empty()
             {
-                return this->send("", 204, {});
+                return this->text("", 204, {});
             }
 
             RuntimeOutput redirect(const std::string &url, const int statusCode = 301, Json::Value headers = {})
             {
                 headers["location"] = url;
-                return this->send("", statusCode, headers);
+                return this->text("", statusCode, headers);
             }
     };
 }
