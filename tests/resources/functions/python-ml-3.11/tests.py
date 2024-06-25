cf377d3291e1c9524a7e07d4d89dcdaeaef44a29
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-06-25 17:16:41 -0400

    test: Python with large binary

--- tests/resources/functions/python-ml-3.11/tests.py
+++ tests/resources/functions/python-ml-3.11/tests.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 import requests
 import os
 import asyncio
@@ -79,6 +80,12 @@ When you can have two!
         return context.res.binary(bytearray([0,40,255])) # Just a filler
     elif action == 'binaryResponse5':
         return context.res.binary(bytearray([0,50,255])) # Just a filler
+    elif action == 'binaryResponseLarge':
+        bytes_body = context.req.body_binary
+        hex = md5(bytes_body).hexdigest()
+        return context.res.send(hex, 200, {
+            'x-method': context.req.method
+        })
     elif action == 'envVars':
         return context.res.json({
             'var': os.environ.get('CUSTOM_ENV_VAR', None),
@@ -88,7 +95,7 @@ When you can have two!
         print('Native log')
         context.log('Debug log')
         context.error('Error log')
-                
+
         context.log("Log+With+Plus+Symbol")
 
         context.log(42)
