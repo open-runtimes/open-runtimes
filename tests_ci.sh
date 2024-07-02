cb0a5ee71ac7d245d6ef6fe08d42b1570815a870
Matej Bačo <matejbaco2000@gmail.com>
2024-07-02 12:44:36 +0000

    Merge CI & DEV tests

--- tests_ci.sh
+++ tests_ci.sh
@@ -1,12 +1,4 @@
-# Usage: sh tests.sh node-21.0
-
-# Configurable varaible for different runtimes
-ID=$1
-ENTRYPOINT=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .ENTRYPOINT" .github/workflows/test.yaml | head -n 1)
-INSTALL_COMMAND=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .OPEN_RUNTIMES_BUILD_COMMAND" .github/workflows/test.yaml | head -n 1)
-START_COMMAND=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .OPEN_RUNTIMES_START_COMMAND" .github/workflows/test.yaml | head -n 1)
-RUNTIME=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .RUNTIME" .github/workflows/test.yaml | head -n 1)
-VERSION=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .VERSION" .github/workflows/test.yaml | head -n 1)
+# Use tests_dev.sh instead, when running locally
 
 # Cleanup
 docker rm --force $(docker ps -aq)
@@ -47,11 +39,11 @@ cd ../../../../
 sleep 10
 OPEN_RUNTIMES_SECRET="test-secret-key" OPEN_RUNTIMES_ENTRYPOINT=$ENTRYPOINT vendor/bin/phpunit --configuration phpunit.xml tests/Base.php
 
-# Dev tests (Uncomment only if fails on CI/CD. Changes rarely make them fail)
+# Dev tests
 
-# docker rm --force $(docker ps -aq)
-# cd ./tests/resources/functions/$RUNTIME
-# docker run -d --name open-runtimes-test-serve -v /tmp/logs:/mnt/logs -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT="$ENTRYPOINT" -e OPEN_RUNTIMES_HEADERS= -e OPEN_RUNTIMES_ENV=development -e OPEN_RUNTIMES_SECRET= -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$START_COMMAND\""
-# cd ../../../../
-# sleep 10
-# vendor/bin/phpunit --configuration phpunit.xml tests/BaseDev.php
+docker rm --force $(docker ps -aq)
+cd ./tests/resources/functions/$RUNTIME
+docker run -d --name open-runtimes-test-serve -v /tmp/logs:/mnt/logs -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT="$ENTRYPOINT" -e OPEN_RUNTIMES_HEADERS= -e OPEN_RUNTIMES_ENV=development -e OPEN_RUNTIMES_SECRET= -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$START_COMMAND\""
+cd ../../../../
+sleep 10
+vendor/bin/phpunit --configuration phpunit.xml tests/BaseDev.php
---
+++ tests_dev.sh
@@ -0,0 +1,11 @@
+# Usage: sh tests.sh node-21.0
+
+# Configurable varaible for different runtimes
+ID=$1
+ENTRYPOINT=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .ENTRYPOINT" .github/workflows/test.yaml | head -n 1)
+INSTALL_COMMAND=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .OPEN_RUNTIMES_BUILD_COMMAND" .github/workflows/test.yaml | head -n 1)
+START_COMMAND=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .OPEN_RUNTIMES_START_COMMAND" .github/workflows/test.yaml | head -n 1)
+RUNTIME=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .RUNTIME" .github/workflows/test.yaml | head -n 1)
+VERSION=$(yq e ".jobs.open-runtimes.strategy.matrix.include[] | select(.ID == \"$ID\") | .VERSION" .github/workflows/test.yaml | head -n 1)
+
+RUNTIME=$RUNTIME ENTRYPOINT=$ENTRYPOINT VERSION=$VERSION INSTALL_COMMAND=$INSTALL_COMMAND START_COMMAND=$START_COMMAND sh tests_ci.sh
