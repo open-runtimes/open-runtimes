cdbe5c2cae11820942c65b48c0d5d54e36abdcc1
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-07-08 13:46:41 -0400

    fix: Python ml

--- runtimes/python/python.dockerfile
+++ runtimes/python/python.dockerfile
@@ -1,4 +1,3 @@
 ENV OPEN_RUNTIMES_ENTRYPOINT=main.py
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
 ENV FLASK_APP="/usr/local/server/src/server.py"
---
+++ runtimes/python/versions/ml-3.11/helpers/after-build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Fail build if any command fails
+set -e
+
+echo "Packing build ..."
+
+# Store entrypoint into build. Will be used during start process
+touch /usr/local/build/.open-runtimes
+echo "OPEN_RUNTIMES_ENTRYPOINT=$OPEN_RUNTIMES_ENTRYPOINT" > /usr/local/build/.open-runtimes
+
+# Append server's dependnecies
+cd /usr/local/server
+pip install --no-cache-dir -r requirements.txt
+
+# Finish build by preparing tar to use for starting the runtime
+tar -C /usr/local/build --exclude code.tar.gz -zcf /mnt/code/code.tar.gz .
+
+echo "Build finished."
