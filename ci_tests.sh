b51651096784664ba9ea93262e452eb91a86cf22
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-07-10 10:03:45 -0400

    chore: Main merge

--- ci_tests.sh
+++ ci_tests.sh
@@ -28,7 +28,7 @@ docker run --rm --name open-runtimes-test-build -v /tmp/.build:/usr/local/server
 # Main tests
 docker run -d --name open-runtimes-test-serve -v /tmp/logs:/mnt/logs -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_HEADERS="{\"x-custom\":\"value\",\"X-CUSTOM-UPPERCASE\":\"value2\",\"x-open-runtimes-custom\":248}" -e OPEN_RUNTIMES_ENTRYPOINT="$ENTRYPOINT" -e OPEN_RUNTIMES_SECRET=test-secret-key -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$START_COMMAND\""
 cd ../../
-sleep 10
+sleep 20
 OPEN_RUNTIMES_SECRET="test-secret-key" OPEN_RUNTIMES_ENTRYPOINT=$ENTRYPOINT vendor/bin/phpunit --configuration phpunit.xml tests/Base.php
 
 # Dev tests
@@ -37,5 +37,5 @@ docker rm --force $(docker ps -aq)
 cd ./tests/.runtime
 docker run -d --name open-runtimes-test-serve -v /tmp/logs:/mnt/logs -v $(pwd)/code.tar.gz:/mnt/code/code.tar.gz:rw -e OPEN_RUNTIMES_ENTRYPOINT="$ENTRYPOINT" -e OPEN_RUNTIMES_HEADERS= -e OPEN_RUNTIMES_ENV=development -e OPEN_RUNTIMES_SECRET= -e CUSTOM_ENV_VAR=customValue -p 3000:3000 open-runtimes/test-runtime sh -c "sh helpers/start.sh \"$START_COMMAND\""
 cd ../../
-sleep 10
+sleep 20
 vendor/bin/phpunit --configuration phpunit.xml tests/BaseDev.php
