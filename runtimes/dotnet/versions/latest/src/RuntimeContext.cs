8b585b0b3272cc4e0918238026e3622c7f6b711b
Matej Bačo <matejbaco2000@gmail.com>
2024-07-31 12:08:51 +0000

    Add formatter for Dotnet, C++

--- runtimes/dotnet/versions/latest/src/RuntimeContext.cs
+++ runtimes/dotnet/versions/latest/src/RuntimeContext.cs
@@ -1,27 +1,26 @@
 namespace DotNetRuntime
 {
-	public class RuntimeContext
-	{
-		public RuntimeRequest Req { get; set; }
-		public RuntimeResponse Res { get; set; }
-		public RuntimeLogger Logger { get; set; }
+    public class RuntimeContext
+    {
+        public RuntimeRequest Req { get; set; }
+        public RuntimeResponse Res { get; set; }
+        public RuntimeLogger Logger { get; set; }
 
-		public RuntimeContext(RuntimeRequest req, RuntimeResponse res, RuntimeLogger logger)
-		{
-			Req = req;
-			Res = res;
-			Logger = logger;
-		}
+        public RuntimeContext(RuntimeRequest req, RuntimeResponse res, RuntimeLogger logger)
+        {
+            Req = req;
+            Res = res;
+            Logger = logger;
+        }
 
-		public void Log(object message)
-		{
-			this.Logger.Write(message, RuntimeLogger.TYPE_LOG);
-		}
+        public void Log(object message)
+        {
+            this.Logger.Write(message, RuntimeLogger.TYPE_LOG);
+        }
 
-		public void Error(object message)
-		{
-			this.Logger.Write(message, RuntimeLogger.TYPE_ERROR);
-		}
-	}
+        public void Error(object message)
+        {
+            this.Logger.Write(message, RuntimeLogger.TYPE_ERROR);
+        }
+    }
 }
-
