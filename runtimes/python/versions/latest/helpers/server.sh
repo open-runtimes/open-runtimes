314089a6e6d2375e5b1c4c8406511019ebc16dec
Matej Bačo <matejbaco2000@gmail.com>
2024-10-17 12:30:41 +0000

    Revert calculation changes

--- runtimes/python/versions/latest/helpers/server.sh
+++ runtimes/python/versions/latest/helpers/server.sh
@@ -1,9 +1,8 @@
 set -e
 
 # 1 worker for each CPU core assigned
-workers=$(echo "$OPEN_RUNTIMES_CPUS" | awk '{print int($1 + 0.999999)}') # Parse float-like string to integer with trick to round-up
-
-echo $workers
+cpu_cores=$(echo "$OPEN_RUNTIMES_CPUS" | awk '{print int($1 + 0.999999)}') # Parse float-like string to integer with trick to round-up
+workers=$((1 + (2 * $cpu_cores)))
 
 echo "HTTP server successfully started!"
 python3 /usr/local/server/src/function/runtime-env/bin/gunicorn -b 0.0.0.0:3000 --log-level='warning' -w $workers --chdir "$(pwd)/src" --worker-class aiohttp.GunicornWebWorker 'server:app'
