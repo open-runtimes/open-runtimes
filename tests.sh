9903af51fdf85ad371f30eb686a2c35c0fda7cf9
Matej Bačo <matejbaco2000@gmail.com>
2024-10-18 10:31:07 +0000

    Revert unwanted changes

--- tests.sh
+++ tests.sh
@@ -4,7 +4,7 @@ set -e
 # Configurable varaible for different runtimes
 ID=$1
 RUNTIME="$(echo $ID | cut -d'-' -f1)"
-VERSION="$(echo $ID | cut -d'-' -f2)-3.11"
+VERSION="$(echo $ID | cut -d'-' -f2)"
 ENTRYPOINT=$(yq ".$RUNTIME.entry" ci/runtimes.toml)
 INSTALL_COMMAND=$(yq ".$RUNTIME.commands.install" ci/runtimes.toml)
 START_COMMAND=$(yq ".$RUNTIME.commands.start" ci/runtimes.toml)
