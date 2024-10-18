c2cedb51384aaf02149d25497adfccc859eff02c
Matej Bačo <matejbaco2000@gmail.com>
2024-10-18 10:12:56 +0000

    Fix Python ML compatibility, and build size

--- runtimes/python/versions/latest/helpers/server.sh
+++ runtimes/python/versions/latest/helpers/server.sh
@@ -5,4 +5,4 @@ cpu_cores=$(echo "$OPEN_RUNTIMES_CPUS" | awk '{print int($1 + 0.999999)}') # Par
 workers=$((1 + (2 * $cpu_cores)))
 
 echo "HTTP server successfully started!"
-python3 /usr/local/server/src/function/runtime-env/bin/gunicorn -b 0.0.0.0:3000 --log-level='warning' -w $workers --chdir "$(pwd)/src" --worker-class aiohttp.GunicornWebWorker 'server:app'
+python3 /usr/local/server/server-env/bin/gunicorn -b 0.0.0.0:3000 --log-level='warning' -w $workers --chdir "$(pwd)/src" --worker-class aiohttp.GunicornWebWorker 'server:app'
