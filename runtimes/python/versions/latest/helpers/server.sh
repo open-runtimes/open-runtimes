5cbaac3bbf274460aa09a28217f1ab9d0767d730
loks0n <22452787+loks0n@users.noreply.github.com>
2024-10-22 13:06:52 +0100

    feat: preload gunicorn

--- runtimes/python/versions/latest/helpers/server.sh
+++ runtimes/python/versions/latest/helpers/server.sh
@@ -11,4 +11,11 @@ if [ "$workers" -eq 0 ]; then
 fi
 
 echo "HTTP server successfully started!"
-python3 /usr/local/server/src/function/runtime-env/bin/gunicorn -b 0.0.0.0:3000 --log-level='warning' -w $workers --chdir "$(pwd)/src" --worker-class aiohttp.GunicornWebWorker 'server:app'
+python3 /usr/local/server/src/function/runtime-env/bin/gunicorn \
+  -b 0.0.0.0:3000 \
+  --log-level='warning' \
+  -w $workers \
+  --chdir "$(pwd)/src" \
+  --worker-class aiohttp.GunicornWebWorker \
+  --preload \
+  'server:app'
