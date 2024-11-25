235ef50bdca9bf0ca475610fd8af6ac05654d30f
Matej Bačo <matejbaco2000@gmail.com>
2024-11-25 14:03:11 +0000

    Improve tests; Remove template leftovers

--- tests/Website.php
+++ tests/Website.php
@@ -9,17 +9,26 @@ class Website extends Base
         parent::setUp();
     }
 
-    public function testStaticFile(): void
+    public function testHomepage(): void
     {
+        // We do not test body content on purpose; HTML is not pre-rendered; almost empty until JS runs
+
         $response = Client::execute(url: '/', method: 'GET');
         self::assertEquals(200, $response['code']);
 
         $response = Client::execute(url: '/index.html', method: 'GET');
         self::assertEquals(200, $response['code']);
+    }
 
-        $response = Client::execute(url: '/favicon.png', method: 'GET');
+    public function testFavicon(): void
+    {
+        $response = Client::execute(url: '/favicon.ico', method: 'GET');
         self::assertEquals(200, $response['code']);
+        self::assertEquals("12e400d2893168e6207185e39df09576", \md5($response['body']));
+    }
 
+    public function testStaticFile(): void
+    {
         $response = Client::execute(url: '/static.txt', method: 'GET');
         self::assertEquals(200, $response['code']);
         self::assertEquals('Sample file', $response['body']);
