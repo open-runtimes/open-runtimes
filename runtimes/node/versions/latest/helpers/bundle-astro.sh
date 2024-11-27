670abcb20291cb7fd19c6d77b2f0d5a78170d60f
Matej Bačo <matejbaco2000@gmail.com>
2024-11-27 12:01:07 +0000

    Add Astro

---
+++ runtimes/node/versions/latest/helpers/bundle-astro.sh
@@ -0,0 +1,4 @@
+set -e
+
+cp package*.json dist/server/
+cp -R node_modules/ dist/server/node_modules/
---
+++ runtimes/node/versions/latest/helpers/server-astro.sh
@@ -0,0 +1,3 @@
+set -e
+
+HOST=0.0.0.0 PORT=3000 node server/entry.mjs # TODO: Security for port 3000
