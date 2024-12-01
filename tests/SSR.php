2c6cefcfde8bcacedb15625d9cc5abbcd45c7849
Matej Bačo <matejbaco2000@gmail.com>
2024-12-01 13:08:08 +0000

    Add sveltekit

--- tests/SSR.php
+++ tests/SSR.php
@@ -18,18 +18,30 @@ class SSR extends CSR
 
     public function testServerAction(): void
     {
+        // Helper for scraping date from the body. Possibly containing irelevant non-dynamic extras.
+        // Do not use with Date or DateTime constructors.
+        $scrapeDate = function(string $body) {
+            // <p id="date">CONTENT</p>
+            $date = \explode('id="date"', $body)[1];
+            $date = \explode('</p>', $body)[0];
+            return $date;
+        };
+
         $response = Client::execute(url: '/date', method: 'GET');
         self::assertEquals(200, $response['code']);
         self::assertNotEmpty($response['body']);
-        $date1 = $response['body'];
+        $body1 = $response['body'];
+        $date1 = $scrapeDate($body1);
 
         \sleep(1);
 
         $response = Client::execute(url: '/date', method: 'GET');
         self::assertEquals(200, $response['code']);
         self::assertNotEmpty($response['body']);
-        $date2 = $response['body'];
+        $body2 = $response['body'];
+        $date2 = $scrapeDate($body2);
 
+        self::assertNotEquals($body1, $body2);
         self::assertNotEquals($date1, $date2);
     }
 }
---
+++ tests/SSR/SvelteKit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\SSR;
+
+use Tests\SSR;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class SvelteKit extends SSR
+{
+}
