c52b27e4f94478bcaecf0073629ca28ebf519a70
Matej Bačo <matejbaco2000@gmail.com>
2024-12-05 20:28:54 +0000

    Speed-up sveltekit cold-start

--- runtimes/node/versions/latest/helpers/sveltekit/bundle.sh
+++ runtimes/node/versions/latest/helpers/sveltekit/bundle.sh
@@ -1,7 +1,3 @@
 set -e
 
-if [ -n "$OPEN_RUNTIMES_OUTPUT_DIRECTORY" ]; then
-    cd $OPEN_RUNTIMES_OUTPUT_DIRECTORY
-fi
-
-mv /usr/local/build/node_modules/ ./node_modules/
+# Server is ready as is (without dependencies)
