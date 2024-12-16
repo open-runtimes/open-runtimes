ecae64a58572bf5739527114608351362001dcb7
Matej Bačo <matejbaco2000@gmail.com>
2024-12-16 17:36:13 +0000

    Fix Astro, WIP :Angular, Nuxt

--- runtimes/node/versions/latest/helpers/astro/server.sh
+++ runtimes/node/versions/latest/helpers/astro/server.sh
@@ -3,4 +3,4 @@ set -e
 cd /usr/local/server/src/function/
 
 # To be used with proxy.sh
-HOST=127.0.0.1 PORT=3001 node server/entry.mjs
+HOST=127.0.0.1 PORT=3001 node -r /usr/local/server/src/logger-ssr.js server/entry.mjs
