b9fc0e99f24834e32f9eac0cb191516fdd21a7d3
Matej Bačo <matejbaco2000@gmail.com>
2024-12-21 18:45:38 +0000

    Analog fix

--- tests/SSR/Analog.php
+++ tests/SSR/Analog.php
@@ -12,13 +12,15 @@ error_reporting(E_ALL);
 class Analog extends SSR
 {
     // Override because Angular uses island architecture, so parts of website were successfully rendered
+    // Analog also doesnt print error message
     public function testServerException(): void
     {
         $response = Client::execute(url: '/exception', method: 'GET');
-        self::assertEquals(200, $response['code']); // Overriden from assertion 500
-        self::assertStringContainsString("<router-outlet></router-outlet>", $response['body']); // New assertion
+        self::assertEquals(200, $response['code']); // Changed
+        self::assertStringContainsString("<router-outlet></router-outlet>", $response['body']); // Added
         self::assertStringNotContainsString("No exceptions", $response['body']);
         self::assertStringNotContainsString('Code exception occured', Client::getLogs('ssr'));
-        self::assertStringContainsString('Code exception occured', Client::getErrors('ssr'));
+        self::assertStringNotContainsString('ERROR Error: NG04002: \'exception\'', Client::getLogs('ssr')); // Added
+        self::assertStringContainsString('ERROR Error: NG04002: \'exception\'', Client::getErrors('ssr')); // Changed
     }
 }
--- tests/resources/functions/analog/vite.config.ts
+++ tests/resources/functions/analog/vite.config.ts
@@ -14,9 +14,17 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     analog({
       ssr: true,
-      static: false,
+      nitro: {
+        routeRules: {
+          '/': {
+            prerender: false,
+          },
+        },
+      },
       prerender: {
-        routes: [],
+        routes: [
+          '/'
+        ],
       },
     }),
   ],
