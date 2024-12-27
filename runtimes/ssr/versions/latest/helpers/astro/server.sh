c14012bb700bd9c180cfdc93821f343be50e83f9
Matej Bačo <matejbaco2000@gmail.com>
2024-12-27 15:58:46 +0000

    Remove SSR proxy

--- runtimes/ssr/versions/latest/helpers/astro/server.sh
+++ runtimes/ssr/versions/latest/helpers/astro/server.sh
@@ -1,7 +1,18 @@
 set -e
 
+echo "OK1"
+
 cd /usr/local/server/src/function/
 
+echo "OK2"
+
 mv ../server-astro.js ./server.js
+
+echo "OK3"
+
+ls -al ../
+
+pwd
+ls -al .
 
 HOST=0.0.0.0 PORT=3000 node ./server.js
