5f3a65ef58ddb86ea898eb7843fa40f40ae11c0d
Matej Bačo <matejbaco2000@gmail.com>
2025-01-10 13:11:57 +0000

    Fix mv after restart

--- runtimes/ssr/versions/latest/helpers/next-js/server.sh
+++ runtimes/ssr/versions/latest/helpers/next-js/server.sh
@@ -2,8 +2,8 @@ set -e
 
 cd /usr/local/server/src/function
 
-mv ../server-next-js.js ./server.js
-mv ../helpers.js ./helpers.js
-mv ../logger.js ./logger.js
+cp ../server-next-js.js ./server.js
+cp ../helpers.js ./helpers.js
+cp ../logger.js ./logger.js
 
 HOST=0.0.0.0 PORT=3000 node ./server.js
