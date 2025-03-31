7e6837e029a02ee644826143f83429686be3e9ba
Matej Bačo <matejbaco2000@gmail.com>
2025-03-31 13:03:02 +0000

    Colorize copy

--- helpers/before-start.sh
+++ helpers/before-start.sh
@@ -3,6 +3,8 @@
 set -e
 shopt -s dotglob
 
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][37m Code extraction started. [0m"
+
 echo "[37mPreparing for start ... [0m"
 
 # Extract gzipped code from mounted volume to function folder
@@ -13,9 +15,17 @@ set -o allexport
 source /usr/local/server/src/function/.open-runtimes
 set +o allexport
 
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][37m Code extraction finished. [0m"
+
 # Enter server folder
 cd /usr/local/server
 
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][37m Environment preparation started. [0m"
+
 . /usr/local/server/helpers/prepare-start.sh
+
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][37m Environment preparation finished. [0m"
+
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][32m Runtime started. [0m"
 
 echo "[37mStarting ... [0m"
