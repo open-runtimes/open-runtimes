667794e4f58eecea9e0d597e40b6ea4e57abc932
Darshan <itznotabug@gmail.com>
2025-05-21 10:37:00 +0530

    update: use non-conflicting names to allow user defined `.env`s as well.

--- runtimes/flutter/versions/latest/helpers/prepare-build.sh
+++ runtimes/flutter/versions/latest/helpers/prepare-build.sh
@@ -5,4 +5,4 @@ shopt -s dotglob
 
 # Flutter's build system does not use the environment variables from current process.
 # So we export vars to an `.env` file that can be consumed using `--dart-define-from-file`.
-printenv | awk -F= '{ print $1 "=\"" substr($0, index($0,$2)) "\"" }' > /usr/local/build/.env
+printenv | awk -F= '{ print $1 "=\"" substr($0, index($0,$2)) "\"" }' > /usr/local/build/.open-runtimes
