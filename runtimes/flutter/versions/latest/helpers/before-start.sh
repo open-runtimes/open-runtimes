8bf610b0a376be1035492e2bd2af83c6cb9fe7c9
Matej Bačo <matejbaco2000@gmail.com>
2025-06-12 16:09:13 +0200

    Support uncompressed builds

--- runtimes/flutter/versions/latest/helpers/before-start.sh
+++ runtimes/flutter/versions/latest/helpers/before-start.sh
@@ -6,7 +6,14 @@ shopt -s dotglob
 echo "Preparing for start ..."
 
 # Extract gzipped code from mounted volume to function folder
-tar -zxf /mnt/code/code.tar.gz -C /usr/local/server/src/function
+if [ -f /mnt/code/code.tar.gz ]; then
+    tar -zxf /mnt/code/code.tar.gz -C /usr/local/server/src/function
+elif [ -f /mnt/code/code.tar ]; then
+    tar -xf /mnt/code/code.tar -C /usr/local/server/src/function
+else
+    echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][97m Code archive not found. [0m"
+    exit 1
+fi
 
 # Enter server folder
 cd /usr/local/server
