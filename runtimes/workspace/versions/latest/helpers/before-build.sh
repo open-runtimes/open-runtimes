bee178b4f053eb81e461a732409882d225083fe9
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-06-25 18:20:11 +0530

    chore: use helper script without source check

---
+++ runtimes/workspace/versions/latest/helpers/before-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Fail build if any command fails
+set -e
+shopt -s dotglob
+
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][97m Environment preparation started. [0m"
+
+# Copy from mounted volume to temporary folder
+cp -R /mnt/code/* /usr/local/build
+
+# Enter build folder
+cd /usr/local/build
+
+. /usr/local/server/helpers/prepare-build.sh
+
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][97m Environment preparation finished. [0m"
+
+# Enter build folder
+cd /usr/local/build
+
+echo "[90m$(date +[%H:%M:%S]) [31m[[00mopen-runtimes[31m][97m Build command execution started. [0m"
+
