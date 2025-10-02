aaa8b814f617727b03636b12ce45124048104c76
Matej Bačo <matejbaco2000@gmail.com>
2025-10-02 16:57:56 +0200

    Fix Deno

--- tests/Serverless/Deno.php
+++ tests/Serverless/Deno.php
@@ -7,11 +7,4 @@ use Tests\Serverless;
 
 class Deno extends Serverless
 {
-    public function testHeadlessBrowser(): void
-    {
-        $response = Client::execute(body: '', headers: ['x-action' => 'headlessBrowser'], timeout: 15);
-        self::assertEquals(200, $response['code']);
-        self::assertEquals('image/png; charset=utf-8', $response['headers']['content-type']);
-        self::assertGreaterThanOrEqual(100000, \mb_strlen($response['body'])); // Should be 1.355MB
-    }
 }
