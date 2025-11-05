02bba2bf19e3c23bf80d759b3776782990d8abfe
Matej Bačo <matejbaco2000@gmail.com>
2025-11-05 12:00:18 +0100

    Fix PHP 8.4

--- ci-runtime-build.sh
+++ ci-runtime-build.sh
@@ -2,5 +2,5 @@ set -e
 shopt -s dotglob
 
 cd ./runtimes/.test
-docker build --platform linux/x86_64 -t open-runtimes/test-runtime .
+docker build --platform linux/x86_64 -t open-runtimes/test-runtime --progress=plain .
 cd ../../
