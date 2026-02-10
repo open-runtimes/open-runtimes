4ad2bbd9a5a2e635212596d9353bbf9af54d5ab7
Hemachandar <hmac.devo@gmail.com>
2026-02-10 22:29:42 +0530

    cleanup

--- runtimes/node/versions/latest/helpers/nuxt/bundle.sh
+++ runtimes/node/versions/latest/helpers/nuxt/bundle.sh
@@ -7,8 +7,6 @@ if [ -n "$OPEN_RUNTIMES_OUTPUT_DIRECTORY" ]; then
 	cd "$OPEN_RUNTIMES_OUTPUT_DIRECTORY"
 fi
 
-printenv
-
 ENTRYPOINT="./server/index.mjs"
 if [ -e "$ENTRYPOINT" ]; then
 	echo -e "\e[90m$(date +[%H:%M:%S]) \e[31m[\e[0mopen-runtimes\e[31m]\e[97m Bundling for SSR started. \e[0m"
