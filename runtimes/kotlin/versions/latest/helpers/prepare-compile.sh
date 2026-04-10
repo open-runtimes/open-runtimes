cfa89bfdaa7a30ead9ed0e015f4f52a92add8828
Jake Barnby <jakeb994@gmail.com>
2026-04-10 22:34:56 +1200

    (style): fix shell formatting (shfmt)

--- runtimes/kotlin/versions/latest/helpers/prepare-compile.sh
+++ runtimes/kotlin/versions/latest/helpers/prepare-compile.sh
@@ -17,11 +17,11 @@ sh gradlew buildJar
 cp -R /usr/local/server/build/libs/* /usr/local/build/compiled
 
 if command -v native-image >/dev/null 2>&1; then
-    echo "Building native image ..."
-    ENTRYPOINT="${OPEN_RUNTIMES_ENTRYPOINT}"
-    CLASS_NAME="${ENTRYPOINT%.*}"
-    CLASS_NAME="${CLASS_NAME//\//.}"
-    cat >/usr/local/build/compiled/reflect-config.json <<REFLECTEOF
+	echo "Building native image ..."
+	ENTRYPOINT="${OPEN_RUNTIMES_ENTRYPOINT}"
+	CLASS_NAME="${ENTRYPOINT%.*}"
+	CLASS_NAME="${CLASS_NAME//\//.}"
+	cat >/usr/local/build/compiled/reflect-config.json <<REFLECTEOF
 [
     {"name": "io.openruntimes.kotlin.${CLASS_NAME}", "allDeclaredMethods": true, "allDeclaredConstructors": true},
     {"name": "io.openruntimes.kotlin.ServerKt", "allDeclaredMethods": true, "allDeclaredConstructors": true},
@@ -29,9 +29,9 @@ if command -v native-image >/dev/null 2>&1; then
     {"name": "kotlin.reflect.jvm.internal.impl.serialization.deserialization.builtins.BuiltInsLoaderImpl", "allDeclaredConstructors": true}
 ]
 REFLECTEOF
-    native-image --no-fallback --enable-url-protocols=http \
-        -H:ReflectionConfigurationFiles=/usr/local/build/compiled/reflect-config.json \
-        -jar /usr/local/build/compiled/kotlin-runtime-1.0.0.jar \
-        -o /usr/local/build/compiled/server
-    rm -f /usr/local/build/compiled/kotlin-runtime-1.0.0.jar /usr/local/build/compiled/reflect-config.json
+	native-image --no-fallback --enable-url-protocols=http \
+		-H:ReflectionConfigurationFiles=/usr/local/build/compiled/reflect-config.json \
+		-jar /usr/local/build/compiled/kotlin-runtime-1.0.0.jar \
+		-o /usr/local/build/compiled/server
+	rm -f /usr/local/build/compiled/kotlin-runtime-1.0.0.jar /usr/local/build/compiled/reflect-config.json
 fi
--- runtimes/kotlin/versions/latest/helpers/server.sh
+++ runtimes/kotlin/versions/latest/helpers/server.sh
@@ -3,7 +3,7 @@ set -e
 shopt -s dotglob
 
 if [ -f src/function/server ]; then
-    ./src/function/server
+	./src/function/server
 else
-    java -jar src/function/kotlin-runtime-1.0.0.jar
+	java -jar src/function/kotlin-runtime-1.0.0.jar
 fi
