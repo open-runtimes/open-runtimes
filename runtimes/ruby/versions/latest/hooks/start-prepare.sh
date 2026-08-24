4858a192231f29e6823f79e4361e4c6b1015641b
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-08-24 16:45:13 +0530

    style(ruby): trim comments

--- runtimes/ruby/versions/latest/hooks/start-prepare.sh
+++ runtimes/ruby/versions/latest/hooks/start-prepare.sh
@@ -8,7 +8,7 @@ if [ -f "/usr/local/server/src/function/Gemfile" ]; then
 	echo "eval_gemfile '/usr/local/server/src/function/Gemfile'" >>/usr/local/server/Gemfile
 fi
 
-# Link dependencies (symlink instead of copy — the bundle can be tens of MB)
+# Link dependencies
 ln -sfn /usr/local/server/src/function/vendor /usr/local/server/vendor
 
 bundle config set --local path 'vendor/bundle'
